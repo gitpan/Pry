9484f264386306d693cb01f14a3a85794849f61a
Toby Inkster <tobyink@cpan.org>
2014-10-05 20:16:18 +0000

    Import of TOBYINK/Pry-0.003000 from CPAN.
    gitpan-cpan-distribution: Pry
    gitpan-cpan-version:      0.003000
    gitpan-cpan-path:         TOBYINK/Pry-0.003000.tar.gz
    gitpan-cpan-author:       TOBYINK
    gitpan-cpan-maturity:     released

--- lib/Pry.pm
+++ lib/Pry.pm
@@ -5,7 +5,7 @@ use warnings;
 package Pry;
 
 our $AUTHORITY = 'cpan:TOBYINK';
-our $VERSION   = '0.002000';
+our $VERSION   = '0.003000';
 
 use Exporter::Shiny our @EXPORT = qw(pry);
 
@@ -19,6 +19,8 @@ BEGIN {
 	}
 };
 
+our ($Lexicals, $Trace, $Already);
+
 # a refinement for the Reply class
 #
 my $_say = sub {
@@ -28,7 +30,27 @@ my $_say = sub {
 	print Term::ANSIColor::colored($text, "bold $colour"), "\n";
 };
 
-our ($Lexicals, $Trace, $Already);
+our $Dumper = 'Data::Dumper';
+
+my $_display_vars = sub {
+	my $invocant = shift;
+	my $_dumper  = $Dumper eq 'Data::Dump'
+		? do { require Data::Dump;   \&Data::Dump::dump }
+		: do { require Data::Dumper; \&Data::Dumper::Dumper };
+	
+	local $Data::Dumper::Deparse = 1;
+	local $Data::Dumper::Terse   = 1;
+	
+	for my $var (@_)
+	{
+		my $val  = ($var =~ /\A\$/) ? ${$Lexicals->{$var}} : $Lexicals->{$var};
+		my $dump = $_dumper->($val);
+		chomp($dump);
+		$dump =~ s/(\A\[)/\(/ and $dump =~ s/(\]\z)/\)/ if $var =~ /\A\@/;
+		$dump =~ s/(\A\{)/\(/ and $dump =~ s/(\}\z)/\)/ if $var =~ /\A\%/;
+		$invocant->$_say("$var = $dump;", "yellow");
+	}
+};
 
 # shim to pass lexicals to Reply
 #
@@ -42,7 +64,7 @@ our ($Lexicals, $Trace, $Already);
 
 # the guts
 #
-sub pry ()
+sub pry (;@)
 {
 	my ($caller, $file, $line) = caller;
 	
@@ -60,7 +82,10 @@ sub pry ()
 	require Reply;
 	require PadWalker;
 	
-	$Lexicals = PadWalker::peek_my(1);
+	$Lexicals = +{
+		%{ PadWalker::peek_our(1) },
+		%{ PadWalker::peek_my(1) },
+	};
 	$Trace = Devel::StackTrace->new(
 		ignore_package => __PACKAGE__,
 		message        => "Prying",
@@ -73,17 +98,22 @@ sub pry ()
 	$repl->step("package $caller");
 	
 	$repl->$_say("Prying at $file line $line", "magenta");
+	$repl->$_display_vars(@_) if @_;
 	$repl->$_say("Current package:   '$caller'");
 	$repl->$_say("Lexicals in scope: @{[ sort keys %$Lexicals ]}");
 	$repl->$_say("Ctrl+D to finish prying.", "magenta");
 	$repl->run;
 	$repl->$_say("Finished prying!", "magenta");
+	
+	my @return = map($Lexicals->{$_}, @_);
+	wantarray ? @return : \@return;
 }
 
 # utils
 #
-sub Lexicals () { $Lexicals if $] }
-sub Trace    () { $Trace    if $] }
+sub Lexicals ()  { $Lexicals if $] }
+sub Trace    ()  { $Trace    if $] }
+sub Dump     (@) { __PACKAGE__->$_display_vars(@_) }
 
 1;
 
@@ -128,7 +158,28 @@ auto-complete of function and variable names.
 Once you've finished using the REPL, just hit Ctrl+D and your code will
 resume execution.
 
-=head1 UTILITIES
+=head2 Functions
+
+=over
+
+=item C<< pry() >>
+
+Starts the Pry REPL.
+
+=item C<< pry(@varnames) >>
+
+Dumps selected variables before starting the Pry REPL.
+
+Note a list of variable I<names> is expected; not I<values>. For
+example:
+
+   my $x = 42;
+   my @y = (666, 999);
+   pry('$x', '@y');
+
+=back
+
+=head3 Utility Functions
 
 The following functions are provided for your convenience. They cannot
 be exported, so you should access them, from the REPL, using their
@@ -143,6 +194,22 @@ Returns a hashref of your lexical variables.
 =item C<< Pry::Trace >>
 
 Returns the stack trace as a L<Devel::StackTrace> object.
+
+=item C<< Pry::Dump(@variable_names) >>
+
+Dumps variables (which must exist somewhere in the C<< Pry::Lexicals >>
+hashref).
+
+=back
+
+=head3 Package Variable
+
+=over
+
+=item C<< $Pry::Dumper >>
+
+Decides the backend dumper implementation used by C<< Pry::Dump() >>.
+Valid values are "Data::Dump" and "Data::Dumper".
 
 =back
 
